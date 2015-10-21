b11f98f1acf69e98d12118b52c0a492025241c78
Nimroot <nimroot@gmail.com>
2015-10-21 23:55:51 +0200

    Window's size and everything else adapts to screen's resolution, so it should fit properly most of the screens (including widescreens and vertical monitors) move around the graph with middle mouse button, code conforms to flake8 as well. Turned off graph zooming, a better solution is in progress.

--- graph-gui.py
+++ graph-gui.py
@@ -1,26 +1,42 @@
 #!/usr/bin/python3
 
 import sys
-from collections     import namedtuple
-from PyQt5.QtWidgets import (QApplication, QWidget, QDesktopWidget,
-    QFrame, QPushButton)
-from PyQt5.QtGui     import QPainter, QColor, QPen
-from PyQt5.QtCore    import Qt, QPoint
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QFrame
+from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5.QtCore import Qt, QPoint
+
 
 class Grapher(QWidget):
+
     def __init__(self):
         super().__init__()
+
+        self.desktop_width = QDesktopWidget().availableGeometry().size().\
+            width()
+        self.desktop_height = QDesktopWidget().availableGeometry().size().\
+            height()
+        """
+        Dimensions of a window should be about 2/3 of a screen's dimension
+        rounded to the nearest 100 to make sure it fits properly.
+        """
+        self.window_width = (round(self.desktop_width * 2 / 300) * 100)
+        self.window_height = (round(self.desktop_height * 2 / 300) * 100)
+
+        if self.window_height > self.window_width:
+            self.grid_size = self.window_width
+        else:
+            self.grid_size = self.window_height
+
         self.initUI()
 
     def initUI(self):
-        self.grid = Grid(self)
-        self.grid.setGeometry(0, 0, 800, 800)
+        self.grid = Grid(self, self.grid_size)
+        self.grid.setGeometry(0, 0, self.grid_size, self.grid_size)
         self.grid.setStyleSheet('QWidget { background-color: #e8e8e8;'
-            'border-right: 1px solid #d5d5d5; }')
-
-        self.resize(1200, 800)
+                                'border-right: 1px solid #d5d5d5; }')
+        self.resize(self.window_width, self.window_height)
         # Move window to the center of the screen
-        window_rec   = self.frameGeometry()
+        window_rec = self.frameGeometry()
         center_point = QDesktopWidget().availableGeometry().center()
         window_rec.moveCenter(center_point)
         self.move(window_rec.topLeft())
@@ -28,31 +44,108 @@ class Grapher(QWidget):
         self.setWindowTitle('Graph Points')
         self.show()
 
+
 class Grid(QFrame):
-    def __init__(self, parent=None):
-        super().__init__(parent);
+    def __init__(self, parent=None, grid_size=0):
+        super().__init__(parent)
 
-        self.grid_scale  = 25
-        self.axis_midpoint = 0
-        self.GPoint = namedtuple('GPoint', ['x', 'y'])
-        self.GPointList = []
+        self.grid_size = grid_size
+        self.grid_scale = grid_size // 20
+        self.axis_midpoint = QPoint(10 * self.grid_scale, 10 * self.grid_scale)
+        self.axis_ticks = 0
 
-        self.GPointList.append(self.GPoint(-9, 1))
-        self.GPointList.append(self.GPoint(-4, -4))
-        self.GPointList.append(self.GPoint(6, -8))
-        self.GPointList.append(self.GPoint(0, 7))
+        self.translate_val = QPoint(0, 0)
+        self.cursor_lastpos = 0.0
+
+        self.PointsList = []
+        self.PointsList.append(QPoint(-9, 1))
+        self.PointsList.append(QPoint(-4, -4))
+        self.PointsList.append(QPoint(6, -8))
+        self.PointsList.append(QPoint(0, 7))
+
+    def mousePressEvent(self, event):
+        self.cursor_lastpos = event.screenPos()
+
+    def mouseMoveEvent(self, event):
+        position_diff = event.screenPos() - self.cursor_lastpos
+
+        if event.buttons() & Qt.MidButton:
+            self.translate_val += position_diff
+            self.cursor_lastpos = event.screenPos()
+            self.axis_midpoint += position_diff
+            self.update()
 
     def wheelEvent(self, event):
-        if event.angleDelta().y() > 0 and self.grid_scale < 50:
-            self.grid_scale += 1
-        if event.angleDelta().y() < 0 and self.grid_scale > 10:
-            self.grid_scale -= 1
+        # if event.angleDelta().y() > 0 and self.grid_scale < 50:
+        #    self.grid_scale += 10
+        # if event.angleDelta().y() < 0 and self.grid_scale > 10:
+        #    self.grid_scale -= 10
 
         self.update()
 
+    def paintEvent(self, event):
+        grid_color = QColor(0, 0, 0)
+        grid_color.setNamedColor('#d5d5d5')
+
+        axis_color = QColor(0, 0, 0)
+        axis_color.setNamedColor('#727272')
+
+        painter = QPainter()
+        painter.begin(self)
+        painter.setRenderHints(QPainter.Antialiasing, True)
+
+        pen = QPen(grid_color, 1, Qt.SolidLine)
+        painter.setPen(pen)
+
+        # Draw grid acording to the scale
+        t = self.translate_val
+        s_point = QPoint(t.x(), t.y())
+        e_point = QPoint(self.grid_size + t.x(),
+                         self.grid_size + t.y())
+
+        print(t.x() // 40)
+
+        while s_point.x() <= e_point.x():
+            painter.drawLine(s_point.x(), t.y(), s_point.x(),
+                             (self.grid_size + t.y()))
+            s_point += QPoint(self.grid_scale, 0)
+
+        while s_point.y() <= e_point.y():
+            painter.drawLine(t.x(), s_point.y(), (self.grid_size + t.x()),
+                             s_point.y())
+            s_point += QPoint(0, self.grid_scale)
+
+        pen.setColor(axis_color)
+        pen.setWidth(1)
+        painter.setPen(pen)
+
+        # Snap axis to the grid
+        if t.x() < 0:
+            painter.drawLine(0, self.axis_midpoint.y(),
+                             (self.grid_size - t.x()),
+                             self.axis_midpoint.y())
+        else:
+            painter.drawLine(0, self.axis_midpoint.y(),
+                             (self.grid_size),
+                             self.axis_midpoint.y())
+
+        if t.y() < 0:
+            painter.drawLine(self.axis_midpoint.x(), 0,
+                             self.axis_midpoint.x(),
+                             (self.grid_size - t.y()))
+        else:
+            painter.drawLine(self.axis_midpoint.x(), 0,
+                             self.axis_midpoint.x(),
+                             self.grid_size)
+
+        for point in self.PointsList:
+            self.plotPoint(point.x(), point.y())
+
+        painter.end()
+
     def plotPoint(self, x, y):
-        x = self.axis_midpoint + self.grid_scale * x
-        y = self.axis_midpoint - self.grid_scale * y
+        x = self.axis_midpoint.x() + self.grid_scale * x
+        y = self.axis_midpoint.y() - self.grid_scale * y
 
         painter = QPainter()
         painter.begin(self)
@@ -62,43 +155,7 @@ class Grid(QFrame):
         point_color.setNamedColor('#7c2136')
         painter.setPen(point_color)
         painter.setBrush(point_color)
-        painter.drawEllipse(QPoint(x,y), 5, 5)
-
-        painter.end()
-
-
-    def paintEvent(self, event):
-        painter = QPainter()
-        painter.begin(self)
-        painter.setRenderHints(QPainter.Antialiasing, True)
-
-        grid_color = QColor(0, 0, 0)
-        grid_color.setNamedColor('#d5d5d5')
-
-        axis_color = QColor(0, 0, 0)
-        axis_color.setNamedColor('#727272')
-
-        pen = QPen(grid_color, 1, Qt.SolidLine)
-        painter.setPen(pen)
-
-        # Draw grid acording to the scale
-        i = 0
-        while i <= 800:
-            painter.drawLine(0, i, 800, i)
-            painter.drawLine(i, 0, i, 800)
-            i += self.grid_scale
-
-        pen.setColor(axis_color)
-        pen.setWidth(1)
-        painter.setPen(pen)
-
-        # Calculate midpoint and snap axis to the grid
-        self.axis_midpoint = ((800 // self.grid_scale) // 2) * self.grid_scale
-        painter.drawLine(0, self.axis_midpoint, 800, self.axis_midpoint)
-        painter.drawLine(self.axis_midpoint, 0, self.axis_midpoint, 800)
-
-        for point in self.GPointList:
-            self.plotPoint(point.x, point.y)
+        painter.drawEllipse(QPoint(x, y), 5, 5)
 
         painter.end()
 
